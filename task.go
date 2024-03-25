3b6cf5c87e44b6d6561a2f6e0f51a0bc8a1cf268
Joe <joe@mi.com>
2024-03-25 12:38:17 +0900

    change RestartPolicy to container.RestartPolicy

--- task.go
+++ task.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"github.com/docker/docker/api/types/container"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -20,19 +21,19 @@ type FileMountConfig struct {
 }
 
 type DeployConfig struct {
-	ImageName     string            `json:"imageName"`
-	ImageTag      string            `json:"imageTag" bson:",omitempty"`
-	ServiceName   string            `json:"serviceName" bson:",omitempty"`
-	MountSource   string            `json:"mountSource" bson:",omitempty"`
-	MountTarget   string            `json:"mountTarget" bson:",omitempty"`
-	FilesToMount  []FileMountConfig `json:"filesToMount" bson:",omitempty"`
-	AutoRemove    bool              `json:"autoRemove"`
-	RestartPolicy string            `json:"restartPolicy" bson:",omitempty"`
-	Env           []string          `json:"env" bson:",omitempty"`
-	HostPort      string            `json:"hostPort" bson:",omitempty"`
-	ExposedPort   string            `json:"exposedPort" bson:",omitempty"`
-	NetworkId     string            `json:"networkId" bson:",omitempty"`
-	NetworkName   string            `json:"networkName" bson:",omitempty"`
+	ImageName     string                  `json:"imageName"`
+	ImageTag      string                  `json:"imageTag" bson:",omitempty"`
+	ServiceName   string                  `json:"serviceName" bson:",omitempty"`
+	MountSource   string                  `json:"mountSource" bson:",omitempty"`
+	MountTarget   string                  `json:"mountTarget" bson:",omitempty"`
+	FilesToMount  []FileMountConfig       `json:"filesToMount" bson:",omitempty"`
+	AutoRemove    bool                    `json:"autoRemove"`
+	RestartPolicy container.RestartPolicy `json:"restartPolicy" bson:",omitempty"`
+	Env           []string                `json:"env" bson:",omitempty"`
+	HostPort      string                  `json:"hostPort" bson:",omitempty"`
+	ExposedPort   string                  `json:"exposedPort" bson:",omitempty"`
+	NetworkId     string                  `json:"networkId" bson:",omitempty"`
+	NetworkName   string                  `json:"networkName" bson:",omitempty"`
 }
 
 type RestartConfig struct {
