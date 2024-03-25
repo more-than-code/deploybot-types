a07506387f8b36cda4c03fe51aeba6e0455c3eec
Joe <joe@mi.com>
2024-03-25 18:40:50 +0900

    refactor

--- common.go
+++ common.go
@@ -1,16 +1,8 @@
 package types
 
-import (
-	"encoding/hex"
-	"errors"
-	"fmt"
-)
+import "encoding/hex"
 
 type ObjectId [12]byte
-type Datetime int64
-
-var NilObjectID ObjectId
-var ErrInvalidHex = errors.New("the provided hex string is not a valid ObjectId")
 
 func (id ObjectId) Hex() string {
 	var buf [24]byte
@@ -18,20 +10,4 @@ func (id ObjectId) Hex() string {
 	return string(buf[:])
 }
 
-func (id ObjectId) String() string {
-	return fmt.Sprintf("ObjectId(%q)", id.Hex())
-}
-
-func ObjectIdFromHex(s string) (ObjectId, error) {
-	if len(s) != 24 {
-		return NilObjectID, ErrInvalidHex
-	}
-
-	var oid [12]byte
-	_, err := hex.Decode(oid[:], []byte(s))
-	if err != nil {
-		return NilObjectID, err
-	}
-
-	return oid, nil
-}
+type Datetime int64
--- constant.go
+++ constant.go
@@ -1,6 +1,59 @@
 package types
 
 const (
+	ExHttpStatusAuthenticationFailure = 460
+	ExHttpStatusBusinessLogicError    = 461
+)
+
+const (
+	CodeClientError      = 1000
+	CodePipelineBusy     = 1100
+	CodePipelineNotFound = 1101
+	CodeTaskBusy         = 1102
+	CodeTaskNotFound     = 1103
+
+	CodeServerError = 2000
+)
+
+const (
+	MsgClientError      = "Client error"
+	MsgPipelineBusy     = "Pipleline busy"
+	MsgPipelineNotFound = "Pipleline not found"
+	MsgTaskBusy         = "Task not found"
+	MsgTaskNotFound     = "Task not found"
+
+	MsgServerError = "Server error"
+)
+
+const (
+	CodeAuthenticationFailure = 100
+
+	CodeInvalidParameters    = 1200
+	CodeWrongEmailOrPassword = 1201
+	CodeDeletedUser          = 1202
+
+	CodeExpiredVerificationCode              = 1100
+	CodeWrongVerificationCode                = 1101
+	CodeTooFrequentlySendingVerificationCode = 1102
+	CodeNeedingResendingVerificationCode     = 1103
+	CodeMaximumAttemptsOnVerificationCode    = 1104
+)
+
+const (
+	MsgAuthenticationFailure = "authentication failure"
+
+	MsgInvalidParamters     = "invalid parameter(s)"
+	MsgWrongEmailOrPassword = "wrong email or password"
+
+	MsgExpiredVerificationCode              = "expired verification code"
+	MsgWrongVerificationCode                = "wrong verification code"
+	MsgDeletedUser                          = "deleted user"
+	MsgTooFrequentlySendingVerificationCode = "too frequently sending code"
+	MsgNeedingResendingVerificationCode     = "needing resending verification code"
+	MsgMaximumAttemptsOnVerificationCode    = "maximum attempts on verification code"
+)
+
+const (
 	TaskPending    = "Pending"
 	TaskInProgress = "InProgress"
 	TaskDone       = "Done"
@@ -32,32 +85,4 @@ const (
 	RoleOwner  Role = "Owner"
 	RoleAdmin  Role = "Admin"
 	RoleMember Role = "Member"
-)
-
-const (
-	CodeAuthenticationFailure = 100
-
-	CodeInvalidParameters    = 1200
-	CodeWrongEmailOrPassword = 1201
-	CodeDeletedUser          = 1202
-
-	CodeExpiredVerificationCode              = 1100
-	CodeWrongVerificationCode                = 1101
-	CodeTooFrequentlySendingVerificationCode = 1102
-	CodeNeedingResendingVerificationCode     = 1103
-	CodeMaximumAttemptsOnVerificationCode    = 1104
-)
-
-const (
-	MsgAuthenticationFailure = "authentication failure"
-
-	MsgInvalidParamters     = "invalid parameter(s)"
-	MsgWrongEmailOrPassword = "wrong email or password"
-
-	MsgExpiredVerificationCode              = "expired verification code"
-	MsgWrongVerificationCode                = "wrong verification code"
-	MsgDeletedUser                          = "deleted user"
-	MsgTooFrequentlySendingVerificationCode = "too frequently sending code"
-	MsgNeedingResendingVerificationCode     = "needing resending verification code"
-	MsgMaximumAttemptsOnVerificationCode    = "maximum attempts on verification code"
 )
