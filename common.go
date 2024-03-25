bb8ca4701708d6fb9b226ae06b3ee6c2b2eec596
Joe <joe@mi.com>
2024-03-25 15:03:45 +0900

    more funcs for ObjectId

--- common.go
+++ common.go
@@ -2,11 +2,15 @@ package types
 
 import (
 	"encoding/hex"
+	"errors"
 	"fmt"
 )
 
 type ObjectId [12]byte
 type Datetime int64
+
+var NilObjectID ObjectId
+var ErrInvalidHex = errors.New("the provided hex string is not a valid ObjectId")
 
 func (id ObjectId) Hex() string {
 	var buf [24]byte
@@ -16,4 +20,18 @@ func (id ObjectId) Hex() string {
 
 func (id ObjectId) String() string {
 	return fmt.Sprintf("ObjectId(%q)", id.Hex())
+}
+
+func ObjectIdFromHex(s string) (ObjectId, error) {
+	if len(s) != 24 {
+		return NilObjectID, ErrInvalidHex
+	}
+
+	var oid [12]byte
+	_, err := hex.Decode(oid[:], []byte(s))
+	if err != nil {
+		return NilObjectID, err
+	}
+
+	return oid, nil
 }
