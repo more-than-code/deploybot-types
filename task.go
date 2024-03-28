12c7a0854226a75706c5fc91f6bb1eddd620ff26
Joe <joe@mi.com>
2024-03-28 19:32:07 +0900

    refactor: move dedicated types away

--- task.go
+++ task.go
@@ -1,38 +1,5 @@
 package types
 
-type BuildConfig struct {
-	ImageName  string             `json:"imageName"`
-	ImageTag   string             `json:"imageTag" bson:",omitempty"`
-	Args       map[string]*string `json:"args" bson:",omitempty"`
-	Dockerfile string             `json:"dockerfile" bson:",omitempty"`
-	RepoUrl    string             `json:"repoUrl"`
-	RepoName   string             `json:"repoName"`
-	RepoBranch string             `json:"repoBranch"`
-}
-
-type RestartPolicy struct {
-	Name              string `json:"name" bson:",omitempty"`
-	MaximumRetryCount int    `json:"maxiumRetryCount" bson:",omitempty"`
-}
-
-type DeployConfig struct {
-	ImageName     string            `json:"imageName"`
-	ImageTag      string            `json:"imageTag" bson:",omitempty"`
-	ServiceName   string            `json:"serviceName" bson:",omitempty"`
-	VolumeMounts  map[string]string `json:"volumeMounts" bson:",omitempty"`
-	Files         map[string]string `json:"files" bson:",omitempty"`
-	AutoRemove    bool              `json:"autoRemove"`
-	RestartPolicy RestartPolicy     `json:"restartPolicy" bson:",omitempty"`
-	Env           []string          `json:"env" bson:",omitempty"`
-	Ports         map[string]string `json:"ports" bson:",omitempty"`
-	NetworkId     string            `json:"networkId" bson:",omitempty"`
-	NetworkName   string            `json:"networkName" bson:",omitempty"`
-}
-
-type RestartConfig struct {
-	ServiceName string `json:"serviceName"`
-}
-
 type Task struct {
 	Id             ObjectId    `json:"id"`
 	Name           string      `json:"name"`
